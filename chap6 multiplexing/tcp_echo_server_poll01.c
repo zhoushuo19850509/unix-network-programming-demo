6151c69ff0fed518043343d1c7508b27e93da290
zhoushuo19850509 <zhoushuo19850509@163.com>
2025-06-14 09:50:07 +0800

    add new code to chap6: 说明pool在IO multiplex中的作用

--- chap6 multiplexing/tcp_echo_server_poll01.c
+++ chap6 multiplexing/tcp_echo_server_poll01.c
@@ -1,8 +1,10 @@
 #include "unp.h"
+#define OPEN_MAX                10240   /* max open files per process - todo, make a config option? */
+
 
 /**
  * 功能： tcp echo server 负责把client发送过来的内容原样发送回去
- * 目标： 为了说明socket server通过select()函数，同时处理多个客户端socket请求的用法
+ * 目标： 为了说明socket server通过 poll()函数，同时处理多个客户端socket请求的用法
  * 环境： playground(imac-home)，这个环境需要预先准备好unp.h
  * 用法： 
  *  1.配置Makefile
@@ -20,10 +22,10 @@
 
 int main(){
 
-    int listenfd; // 监听的socket
-    int maxfd; 
-    int sockfd;
-    int client[FD_SETSIZE]; // 这个数组存放的是所有客户端连接socket fd
+    int listenfd; // server socket listening fd
+    int sockfd;    // 保存在client数组中来自客户端的socketfd
+    int connectfd; // 接收到来自客户端的socket连接
+    struct pollfd client[OPEN_MAX]; // 这个数组存放的是poll检测到的socket poll fd
     int maxi;
     int i;
     ssize_t n;
@@ -33,11 +35,9 @@ int main(){
      *      也可是客户端通过之前已经连接的socket fd发送数据过来
     */
     int nready;
-    fd_set allset; // 登记所有连接上来的client socket fd
-    fd_set rset;
     char buf[MAXLINE];
 
-    int connectfd; // 接收到来自客户端的socket连接
+    
     socklen_t clilen; // length of client address length
 
     struct sockaddr_in servaddr; // socket server address
@@ -54,21 +54,24 @@ int main(){
     servaddr.sin_port = htons(SERV_PORT);  
 
     // 3.绑定端口(bind)
-    bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+    Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
     // 4.监听端口(listen)
-    listen(listenfd, LISTENQ);
+    Listen(listenfd, LISTENQ);
     printf("start listening on port: %d", SERV_PORT);
 
-    maxfd = listenfd;
-    maxi = -1;
-    // 初始化fd client set
-    for(i = 0; i < FD_SETSIZE; i++){
-        client[i] = -1;
+    /**
+     * 把server socket 登记到client数组第一个元素
+     * 从这里我们也可以看到client数组元素： pollfd的组成
+    */
+    client[0].fd = listenfd;
+    client[0].events = POLLRDNORM;
+    // 初始化client数组各个元素： pollfd
+    for(i = 0; i < OPEN_MAX; i++){
+        client[i].fd = -1;
     }
 
-    FD_ZERO(&allset);
-    FD_SET(listenfd, &allset);
+    maxi = 0;
 
     /**
      * 5.循环，这个循环做两个事情
@@ -78,15 +81,13 @@ int main(){
      *   处理客户端通过原有socket连接发送过来的数据
     */
     for(;;){
-        rset = allset;
-
         /**
          * Select()既可以处理新的socket连接请求，
          * 也可以处理通过原有socket请求管道发送过来的数据
          * 
         */
-        nready = Select(maxfd + 1, &rset, NULL, NULL, NULL);
-        printf("current nready: %d" , nready);
+        nready = Poll(client, maxi + 1, INFTIM);
+        // printf("current nready: %d" , nready);
         
         /**
          * 代码块1
@@ -101,28 +102,24 @@ int main(){
          * 3.client socket fd登记到数据client中
          * 
         */
-        if(FD_ISSET(listenfd, &rset)){
+        if(client[0].revents & POLLRDNORM ){
             clilen = sizeof(cliaddr);
             // connectfd是最新连接上来的socket fd
             connectfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
             
             // 这个for循环是从client数组中找一个空位，存放新socket连接 fd
-            for(i = 0; i < FD_SETSIZE; i++){
-                if(client[i] < 0){
-                    client[i] = connectfd;
+            for(i = 1; i < OPEN_MAX; i++){
+                if(client[i].fd < 0){
+                    client[i].fd = connectfd;
                     break;
                 }
             }
             // 遍历client数组，已经没有空位了，没法再接收新的client socket连接请求了
-            if(i == FD_SETSIZE){
+            if(i == OPEN_MAX){
                 err_quit("too many clients...");
             }
-            // 把新连接登记到allset中
-            FD_SET(connectfd, &allset);
-            // 更新maxfd，maxfd保存的是所有连接上来的client socket fd中最大的那个编号
-            if(connectfd > maxfd){
-                maxfd = connectfd;
-            }
+            client[i].events = POLLRDNORM;
+
             // 更新maxi，maxi保存的是最新的连接上来的client socket在client数组中的index
             if(i > maxi){
                 maxi = i;
@@ -130,7 +127,7 @@ int main(){
             /**
              * nready的含义我们已经清楚了(参考nready定义处的注释)
              * 这里if()判断的意思是，
-             * Select()的结果nready如果为1 ，就意味着这次Select()接收到一个新的socket请求
+             * poll()的结果nready如果为1 ，就意味着这次poll()接收到一个新的socket请求
              * 那么，在代码块1 中处理完本次新socket请求之后nready就归0了，
              * 在这种情况下，后续代码块2 for循环就没必要继续处理了
             */
@@ -148,20 +145,28 @@ int main(){
          * 4.如果Read()不到数据，说明socket关闭，就清理这个socket fd
          * 如果Read()读取到了数据，就把数据echo back to the client
         */
-        for(i = 0; i <= maxi; i++){
+        for(i = 1; i <= maxi; i++){
             // 先剔除掉fd array中空的元素，不处理
-            if( (sockfd = client[i]) < 0){
+            if( (sockfd = client[i].fd) < 0){
                 continue;
             }
             /**
              * 然后处理实际有可读内容的socket
              * 读取完成后关闭socket
             */
-            if(FD_ISSET(sockfd, &rset)){
-                if((n = Read(sockfd, buf, MAXLINE)) == 0){
+            if(client[i].revents & (POLLRDNORM | POLLERR)){
+                if((n = read(sockfd, buf, MAXLINE)) < 0){
+                    if(errno == ECONNRESET){ // connect reset by client
+                        Close(sockfd);
+                        client[i].fd = -1;
+                    }else{
+                        err_sys("read error");
+                    }
+                }else if(n == 0){
+                    // connect closed by client
                     Close(sockfd);
-                    FD_CLR(sockfd, &allset);
-                    client[-1] = -1;
+                    client[i].fd = -1;
+                    
                 }else{
                     // 把接收到的数据立即通过socket返回给客户端
                     Write(sockfd, buf, n);
